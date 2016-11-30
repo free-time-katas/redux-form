8c9683138d88bddaa175fc6d3b77f1cd443bb9a5
David Díaz <diazdavid.info@gmail.com>
2016-11-30 09:11:47 +0100

    add validate simple form

--- build/components/simpleForm.jsx
+++ build/components/simpleForm.jsx
@@ -7,24 +7,10 @@ class SimpleForm extends React.Component {
         const {handleSubmit, pristine, reset, submitting} = this.props;
         return (
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>First Name</label>
-                    <div>
-                        <Field name="firstName" component="input" type="text" placeholder="First Name"/>
-                    </div>
-                </div>
-                <div>
-                    <label>Last Name</label>
-                    <div>
-                        <Field name="lastName" component="input" type="text" placeholder="Last Name"/>
-                    </div>
-                </div>
-                <div>
-                    <label>Email</label>
-                    <div>
-                        <Field name="email" component="input" type="email" placeholder="Email"/>
-                    </div>
-                </div>
+                <Field name="firstName" component={renderField} type="text" placeholder="First Name"
+                       label="First Name"/>
+                <Field name="lastName" component={renderField} type="text" placeholder="Last Name" label="Last Name"/>
+                <Field name="email" component={renderField} type="email" placeholder="Email" label="Email"/>
                 <div>
                     <label>Sex</label>
                     <div>
@@ -32,17 +18,12 @@ class SimpleForm extends React.Component {
                         <label><Field name="sex" component="input" type="radio" value="female"/> Female</label>
                     </div>
                 </div>
-                <div>
-                    <label>Favorite Color</label>
-                    <div>
-                        <Field name="favoriteColor" component="select">
-                            <option></option>
-                            <option value="ff0000">Red</option>
-                            <option value="00ff00">Green</option>
-                            <option value="0000ff">Blue</option>
-                        </Field>
-                    </div>
-                </div>
+                <Field name="favoriteColor" component={renderField} label="Favorite Color">
+                    <option></option>
+                    <option value="ff0000">Red</option>
+                    <option value="00ff00">Green</option>
+                    <option value="0000ff">Blue</option>
+                </Field>
                 <div>
                     <label htmlFor="employed">Employed</label>
                     <div>
@@ -64,6 +45,46 @@ class SimpleForm extends React.Component {
     }
 }
 
+const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <input {...input} placeholder={label} type={type}/>
+            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+);
+
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required'
+    } else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 characters or less'
+    }
+    if (!values.lastName) {
+        errors.lastName = 'Required'
+    } else if (values.lastName.length < 2) {
+        errors.lastName = 'Must be 2 characters or more'
+    }
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
+    return errors
+};
+
+const warn = values => {
+    const warnings = {};
+    if (!values.favoriteColor) {
+        warnings.favoriteColor = 'Hmm, you don\'t select favorite color'
+    }
+    return warnings
+};
+
 export default reduxForm({
-    form: 'simple'  // a unique identifier for this form
+    form: 'simple',  // a unique identifier for this form
+    validate,
+    warn
 })(SimpleForm)
